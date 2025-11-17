7968a1f8143d6a89b85212a42add731ba0a95bbf
Momade Junior <momadejunior584@gmail.com>
2025-11-17 12:16:30 +0200

    update all projects pages

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,12 +5,12 @@ import Image from 'react'
 
 export default function Navbar() {
   return (
-    <header className="flex justify-between items-center px-10 py-6 sticky top-0 bg-white bg-opacity-90 shadow-md z-10">
+    <header className="flex justify-between items-center px-10 py-6 sticky top-0 bg-white bg-opacity-90 shadow-md z-10" id="navbar">
       <Link to="/">
          <img
             src="./logo.png"
-            alt="App Preview"
-            className='w-18'
+            alt="VuyaKaya"
+            className='w-32'
           />
       </Link>
       <nav className="space-x-6">
--- src/pages/home.js
+++ src/pages/home.js
@@ -20,9 +20,17 @@ export default function Home() {
           <a
             href="https://github.com/momadejunior/vuyakaya.realease/releases/download/v1.0.0/vuyakaya.apk"
             download
-            className="inline-block bg-white text-[#DA1C5C] px-6 py-3 rounded-full font-semibold hover:bg-gray-100 transition transform hover:scale-105"
+            className="inline-block bg-white text-[#DA1C5C] px-6 py-3 rounded-full font-semibold hover:bg-gray-100 transition transform hover:scale-105 mr-2"
           >
             Baixar para Android
+          </a>
+
+           <a
+            href="/signin"
+        
+            className="inline-block bg-white text-[#DA1C5C] px-6 py-3 rounded-full font-semibold hover:bg-gray-100 transition transform hover:scale-105"
+          >
+            Entrar
           </a>
         </div>
         <div className="justify-center md:w-1/2 flex">
